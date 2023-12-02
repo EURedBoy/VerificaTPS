2ebdca25a5b2172d309fa2fa9d240c2c1328047c
marco rossetti <marco.rossetti@issgreppi.it>
2023-12-02 09:10:25 +0100

    Risoluzione secondo problema immagine nel js

--- page/Accomodation/script.js
+++ page/Accomodation/script.js
@@ -3,6 +3,6 @@ const textElement = document.getElementById('mainText');
 
 
 setTimeout(() => {
-    imageElement.src = 'nuova_immagine.jpg';
+    imageElement.src = '../../src/nuova_immagine.jpg';
     textElement.innerText = 'Il piacere dello studio hahah';
 }, 5000);
